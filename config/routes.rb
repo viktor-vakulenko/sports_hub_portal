49f4604c1070f7684d7435594a356c8e81005fa0
viktor <irvin1442@gmail.com>
2022-01-17 12:57:31 +0200

    Add gem Devise and url to logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'persons/profile', as: 'user_root'
   resources :articles
   get 'home/index'
-  root 'home#index'
+  root to: 'home#index'
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+    get 'sign_in' => 'devise/sessions#new'
+    get 'persons/profile' => 'devise/persons#profile'
+  end
 end
