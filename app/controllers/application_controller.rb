543bdb3c91f9f57fb0c295f8462c0bfe0dd1922c
viktor <irvin1442@gmail.com>
2022-01-18 14:32:41 +0200

    Add first_name , last_name fields for edit profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   def after_sign_in_path_for(resource)
     root_path
+  end
+
+  protect_from_forgery with: :exception
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
   end
 end
