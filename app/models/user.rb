bb980cc8a4dd2ab231fe6e6b7dfd24fd0a20bc23
viktor <irvin1442@gmail.com>
2022-01-19 15:24:08 +0200

    Add validation for last name and first name fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :last_name, presence: true
+  validates :first_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
