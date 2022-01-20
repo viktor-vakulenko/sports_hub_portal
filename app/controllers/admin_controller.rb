f5dfc712234a1aa79cc7f5410d19e344b2a6c1b1
viktor <irvin1442@gmail.com>
2022-01-20 16:10:05 +0200

    Add admin layout

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class AdminController < ApplicationController
+  layout 'admin'
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  layout :determine_layout
 
   def after_sign_in_path_for(resource)
     root_path
@@ -8,11 +8,22 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  def is_admin?
+    if user_signed_in?
+      redirect_to root_path unless current_user.admin
+    end
+  end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
+  end
+
+  private
+
+  def determine_layout
+    current_user.admin ? 'admin' : 'application'
   end
 end
