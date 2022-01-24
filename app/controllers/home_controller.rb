a1fde73b6329fa1c4867cbda4da34263c52648dd
viktor <irvin1442@gmail.com>
2022-01-24 14:40:31 +0200

    Change layout view and add CRUD actions  articles for admin namespace

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!
+  before_action :set_homepage, only: %i[show edit update destroy]
+  # skip_before_action :authenticate_user!
+
   def index
+    @homepage = Home.new
   end
+
+  def set_homepage
+    @homepage = Home.find(params[:id])
+  end
+
+  def edit
+  end
+
 end
