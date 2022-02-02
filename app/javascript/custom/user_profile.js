cb3d520684ccef8f8e963d4300b6b538f5482421
viktor <irvin1442@gmail.com>
2022-02-02 15:03:57 +0200

    Created Home page html layout

--- app/javascript/custom/user_profile.js
+++ app/javascript/custom/user_profile.js
@@ -6,4 +6,13 @@ jQuery(() => {
         // on mouseout, reset the background colour
         $('#blur').removeClass('blur')
     });
+
+    $(".on-hover-shadow").hover(
+        function () {
+            $(this).addClass("shadow");
+        },
+        function () {
+            $(this).removeClass("shadow");
+        }
+    );
 });
