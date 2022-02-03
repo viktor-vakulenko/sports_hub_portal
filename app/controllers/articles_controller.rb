03666195888576cce4a380cf4e56839dd2e54b28
viktor <irvin1442@gmail.com>
2022-02-03 11:38:14 +0200

    Remove not used method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,6 @@ class ArticlesController < ApplicationController
   # GET /articles/1 or /articles/1.json
   def show
     add_breadcrumbs('Articles', category_articles_path(16), true)
-    # @article.increase_visit
     Article.increment_counter(:visit_counter, @article.id)
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,4 @@
 class Article < ApplicationRecord
   belongs_to :category, optional: true
   has_one_attached :image
-
-  def increase_visit
-    visit_counter = + 1
-    save!
-  end
-
 end
